1913e387aad306c24db272f7034ca2f18680a563
Matthew Pickering <matthewtpickering@gmail.com>
2017-08-08 11:04:10 +0000

    Changes to make the build work, plugin does not compile

--- kythe-proto/default.nix
+++ kythe-proto/default.nix
@@ -1,8 +1,14 @@
-{ mkDerivation, base, Cabal, proto-lens-protoc, stdenv }:
+{ mkDerivation, base, Cabal, proto-lens-protoc, stdenv, kythe }:
 mkDerivation {
   pname = "kythe-proto";
   version = "0.1.0.0";
   src = ./.;
+  postUnpack = ''
+    rm -r $sourceRoot/third_party
+    mkdir -pv $sourceRoot/third_party/kythe
+    cp -r ${kythe} $sourceRoot/third_party/kythe/kythe
+    chmod 755 -R $sourceRoot/third_party/kythe
+  '';
   setupHaskellDepends = [ base Cabal proto-lens-protoc ];
   libraryHaskellDepends = [ base proto-lens-protoc ];
   homepage = "https://github.com/google/haskell-indexer";
